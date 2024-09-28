9ac048da5f2936a67e13d72836f7beebd6fc31b7
Kazumasa Utashiro <kaz@utashiro.com>
2024-09-28 15:42:27 +0900

    make generic read_asc() function

--- lib/App/optex/pingu/Picture.pm
+++ lib/App/optex/pingu/Picture.pm
@@ -8,7 +8,7 @@ use Exporter 'import';
 our @EXPORT_OK = qw(&load);
 
 use Data::Dumper;
-use List::Util qw(pairs zip reduce);
+use List::Util qw(pairs zip reduce all);
 use Term::ANSIColor::Concise qw(ansi_color);
 $Term::ANSIColor::Concise::NO_RESET_EL = 1;
 
@@ -41,14 +41,14 @@ sub load {
 	read_asc($_);
     }
     elsif ($file =~ /\.asc2$/) {
-	read_asc2($_);
+	read_asc({ y => 2 }, $_);
     }
     elsif ($file =~ /\.asc4$/) {
-	read_asc4($_);
+	read_asc({ x => 2, y => 2 }, $_);
     }
 }
 
-sub read_asc {
+sub read_asc_1 {
     local $_ = shift;
     s/^#.*\n//mg;
     s{ (?<str>(?<col>$color_re)\g{col}*) }{
@@ -62,11 +62,10 @@ sub read_asc {
 sub squeeze {
     map @$_, reduce {
 	my $x = $a->[-1];
-	if ($x && $x->[0] eq $b->[0] && $x->[1] eq $b->[1]) {
-	    $x->[2]++;
+	if ($x && all { $x->[$_] eq $b->[$_] } 0 .. $#{$b}) {
+	    $x->[-1]++;
 	} else {
-	    $b->[2] = 1;
-	    push @$a, $b;
+	    push @$a, [ @$b, 1 ];
 	}
 	$a;
     } [], @_;
@@ -75,7 +74,7 @@ sub squeeze {
 my $use_FB  = 0; # use FULL BLOCK when upper/lower are same
 my $use_BHB = 0; # use LOWER HALF BLOCK to show lower part
 
-sub stringify {
+sub stringify_2 {
     my($hi, $lo, $c) = @{+shift};
     $c //= 1;
     if ($hi =~ $color_re) {
@@ -99,7 +98,7 @@ sub stringify {
     }
 }
 
-sub read_asc2 {
+sub read_asc_2 {
     my $data = shift;
     my @data = grep !/^\s*#/, $data =~ /.+/g;
     @data % 2 and die "Data format error.\n";
@@ -107,7 +106,7 @@ sub read_asc2 {
     for (pairs @data) {
 	my($hi, $lo) = @$_;
 	my @data = squeeze zip [ $hi =~ /\X/g ], [ $lo =~ /\X/g ];
-	my $line = join '', map stringify($_), @data;
+	my $line = join '', map stringify2($_), @data;
 	push @image, $line;
     }
     wantarray ? @image : join('', map "$_\n", @image);
@@ -115,40 +114,46 @@ sub read_asc2 {
 
 ######################################################################
 
-my @quadrant = (
-    '',    # 0000
-    QLR,   # 0001
-    QLL,   # 0010
-    BHB,   # 0011
-    QUR,   # 0100
-    RHB,   # 0101
-    QURLL, # 0110
-    QULx,  # 0111
-    QUL,   # 1000
-    QULLR, # 1001
-    LHB,   # 1010
-    QURx,  # 1011
-    THB,   # 1100
-    QLLx,  # 1101
-    QLRx,  # 1110
-    FB,	   # 1111
+my %elements = (
+    "0"    => '',    #
+    "1"    => FB,    #
+    "00"   => '',    #
+    "10"   => THB,   #
+    "01"   => BHB,   #
+    "11"   => FB,    #
+    "0000" => '',    #
+    "0001" => QLR,   #
+    "0010" => QLL,   #
+    "0011" => BHB,   #
+    "0100" => QUR,   #
+    "0101" => RHB,   #
+    "0110" => QURLL, #
+    "0111" => QULx,  #
+    "1000" => QUL,   #
+    "1001" => QULLR, #
+    "1010" => LHB,   #
+    "1011" => QURx,  #
+    "1100" => THB,   #
+    "1101" => QLLx,  #
+    "1110" => QLRx,  #
+    "1111" => FB,    #
 );
 
-sub stringify4 {
-    my($hi, $lo, $n) = @{+shift};
-    my $quad = $hi . $lo;
-    my $fg = ($quad =~ /($color_re)/)[0] // '';
-    my $bg = ($quad =~ /([^$fg\n])/)[0] // '';
-    my $ch = (state $cache = {})->{$quad} //= do {
+sub stringify {
+    my $vec = shift;
+    my $n = pop @$vec;
+    my $spec = join '', @$vec;
+    my $fg = ($spec =~ /($color_re)/)[0] // '';
+    my $bg = ($spec =~ /((?!$fg).)/)[0] // '';
+    my $ch = (state $cache = {})->{$spec} //= do {
 	if (!$fg) {
-	    substr $hi, 0, 1;
+	    substr $spec, 0, 1;
 	} else {
-	    my $bit = $quad =~ s/(.)/int($1 eq $fg)/ger;
-	    my $index = oct "0b$bit";
-	    $quadrant[$index] // die;
+	    my $bit = $spec =~ s/(.)/int($1 eq $fg)/ger;
+	    $elements{$bit} // die "$bit";
 	}
     };
-    my $s = $ch x $n // 1;
+    my $s = $ch x $n || 1;
     if (my $color = $fg) {
 	$color .= "/$bg" if $bg =~ /$color_re/;
 	$s = ansi_color($color, $s);
@@ -156,15 +161,17 @@ sub stringify4 {
     $s;
 }
 
-sub read_asc4 {
+sub read_asc {
+    my $opt = ref $_[0] eq 'HASH' ? shift : {};
+    my $x = $opt->{x} // 1;
+    my $y = $opt->{y} // 1;
     my $data = shift;
     my @data = $data =~ /.+/g;
     @data % 2 and die;
     my @image;
-    for (pairs @data) {
-	my($hi, $lo) = @$_;
-	my @data = squeeze zip [ $hi =~ /\X\X/g ], [ $lo =~ /\X\X/g ];
-	my $line = join '', map stringify4($_), @data;
+    while (my @y = splice(@data, 0, $y)) {
+	my @data = squeeze zip map { [ /\X{$x}/g ] } @y;
+	my $line = join '', map stringify($_), @data;
 	push @image, $line;
     }
     wantarray ? @image : join('', map "$_\n", @image);
